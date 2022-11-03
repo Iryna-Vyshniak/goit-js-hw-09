a60f4f693ab64f63c3d53168e8e8cdc15a71830e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-11-04 00:43:33 +0200

    add script to color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,28 @@
+const CHANGE_COLOR_DELAY = 1000;
+let idInt = null;
 
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body: document.body,
+}
 
+refs.btnStart.addEventListener('click', onBtnStartChangeColor);
+refs.btnStop.addEventListener('click', onBtnStopChangeColor);
 
+function onBtnStartChangeColor() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+    idInt = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, CHANGE_COLOR_DELAY);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function onBtnStopChangeColor() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+    clearInterval(idInt);
+}
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;

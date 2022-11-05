aa0b711abb48e9037634d1fd3f3cadc32a927635
Iryna-Vyshniak <111734415+Iryna-Vyshniak@users.noreply.github.com>
2022-11-05 20:17:28 +0000

    Deploying to gh-pages from @ Iryna-Vyshniak/goit-js-hw-09@55c8f45a540fab20391291f950bfd7f526e44982 🚀

---
+++ 03-promises.60c9e5a4.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnSubmit:document.querySelector("button[type='submit']")};t.body.style.backgroundColor="#f7eff4",t.btnSubmit.disabled=!0;
+//# sourceMappingURL=03-promises.60c9e5a4.js.map

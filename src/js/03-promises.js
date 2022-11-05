55c8f45a540fab20391291f950bfd7f526e44982
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-11-05 22:16:57 +0200

    modified styles and add some script

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,20 @@
+
+const refs = {
+  body: document.querySelector('body'),
+  btnSubmit: document.querySelector("button[type='submit']"),
+}
+
+refs.body.style.backgroundColor = '#f7eff4';
+refs.btnSubmit.disabled = true;
+
+
+
+
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {

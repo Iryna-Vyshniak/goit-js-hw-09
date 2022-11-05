e67181b9808531f550573f01cf23d4422f6c15ef
Iryna-Vyshniak <111734415+Iryna-Vyshniak@users.noreply.github.com>
2022-11-05 15:50:03 +0000

    Deploying to gh-pages from @ Iryna-Vyshniak/goit-js-hw-09@d5e1fda8e52f160cf18796f8202635af917567ce 🚀

--- 01-color-switcher.51ef570a.js
+++
@@ -1,2 +0,0 @@
-let t=null;const e={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.body};e.btnStart.addEventListener("click",(function(){e.btnStart.disabled=!0,e.btnStop.disabled=!1,t=setInterval((()=>{e.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.btnStop.addEventListener("click",(function(){e.btnStart.disabled=!1,e.btnStop.disabled=!0,clearInterval(t)}));
-//# sourceMappingURL=01-color-switcher.51ef570a.js.map
